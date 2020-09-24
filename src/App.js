9b00ade6e3624d8664bcb2fe7045cbe923576369
stbugyei <stbugyei@yahoo.co.uk>
2020-09-24 03:24:45 +0300

    removed 404 pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import FetchWeather from './component/FetchWeather'
 import './App.css';
 
 
 function App() {
   return (
-    <BrowserRouter forceRefresh={true} basename={process.env.PUBLIC_URL + '/'}>
+    <Router basename={process.env.PUBLIC_URL}>
       <FetchWeather />
-    </BrowserRouter>
+    </Router>
   );
 }
 
